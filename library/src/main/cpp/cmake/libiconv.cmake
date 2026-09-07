fac666db5e6395b02c09bd628f3ef5983175bd87
Amnesia <amnesia@example.com>
2026-09-07 00:00:21 -0400

    fix: libiconv remove debug echo that broke CMake quoting
    The echo libiconv: cygpath check inside bash -c had inner
    double quotes and : that caused 'Argument not separated by
    whitespace' at column 145/264/278 in ep_libiconv-configure-Debug.cmake.
    Revert to minimal if cygpath; then src=...; cd src && ./configure ...

--- library/src/main/cpp/cmake/libiconv.cmake
+++ library/src/main/cpp/cmake/libiconv.cmake
@@ -11,7 +11,7 @@ ExternalProject_Add(ep_libiconv
     LOG_INSTALL 1
     CONFIGURE_COMMAND
         "${BASH_EXECUTABLE}" -c
-        "set -ex; echo libiconv: cygpath check; if command -v cygpath >/dev/null 2>&1; then src=$(cygpath -u \"<SOURCE_DIR>\"); else src=\"<SOURCE_DIR>\"; fi; echo libiconv src=\$src; cd \"$src\" && ./configure ${EP_AUTOTOOLS_ARGS_STR} --enable-extra-encodings"
+        "if command -v cygpath >/dev/null 2>&1; then src=$(cygpath -u \"<SOURCE_DIR>\"); else src=\"<SOURCE_DIR>\"; fi; cd \"$src\" && ./configure ${EP_AUTOTOOLS_ARGS_STR} --enable-extra-encodings"
     BUILD_COMMAND ${Make_EXECUTABLE} -j1 V=1
     INSTALL_COMMAND ${Make_EXECUTABLE} -j1 install V=1
 )
