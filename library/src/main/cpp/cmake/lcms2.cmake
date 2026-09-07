a6a641c31a249f8b2a7041a14c24048fe8db7b7f
Amnesia <amnesia@example.com>
2026-09-07 00:27:42 -0400

    fix: use release tarballs for lcms2/libffi to avoid autotools
    Git clones require ./autogen.sh which needs autoconf/automake/libtool —
    not present in MSYS2 by default (error: You must have autoconf/libtool/
    automake installed). Use pre-generated release tarballs that already
    contain ./configure:
      lcms2: lcms2-2.19.tar.gz from SourceForge (SHA256 a0d9cc...)
      libffi: libffi-3.6.0.tar.gz from GitHub releases (SHA256 9cc832...)
    This removes the autoreconf dependency and allows the helper scripts to
    simply cd and exec ./configure with EP_AUTOTOOLS_ARGS.

--- library/src/main/cpp/cmake/lcms2.cmake
+++ library/src/main/cpp/cmake/lcms2.cmake
@@ -1,12 +1,11 @@
 include(ExternalProject)
 
 ExternalProject_Add(ep_lcms2
-    GIT_REPOSITORY https://github.com/mm2/Little-CMS
-    GIT_TAG lcms2.19.1
-    GIT_SHALLOW TRUE
+    URL https://downloads.sourceforge.net/project/lcms/lcms/2.19/lcms2-2.19.tar.gz
+    URL_HASH SHA256=a0d9cc6b6f0f334fa1ced499d42298a4b89dccf6f745a4a5072d1b76929522d1d4
     BUILD_IN_SOURCE true
     CONFIGURE_COMMAND
-        "${BASH_EXECUTABLE}" "${AUTOTOOLS_HELPER_AUTOGEN_MSYS}" "<SOURCE_DIR>" ${EP_AUTOTOOLS_ARGS}
+        "${BASH_EXECUTABLE}" "${AUTOTOOLS_HELPER_MSYS}" "<SOURCE_DIR>" ./configure ${EP_AUTOTOOLS_ARGS}
     BUILD_COMMAND ${Make_EXECUTABLE} -j${NPROC} all
     INSTALL_COMMAND ${Make_EXECUTABLE} -j${NPROC} install
 )
--- library/src/main/cpp/cmake/libffi.cmake
+++ library/src/main/cpp/cmake/libffi.cmake
@@ -1,12 +1,11 @@
 include(ExternalProject)
 
 ExternalProject_Add(ep_libffi
-    GIT_REPOSITORY https://github.com/libffi/libffi
-    GIT_TAG v3.6.0
-    GIT_SHALLOW TRUE
+    URL https://github.com/libffi/libffi/releases/download/v3.6.0/libffi-3.6.0.tar.gz
+    URL_HASH SHA256=9cc832d54f2860b490fa8a0a0d26d742084bac394d3c4b9e4b90cb92ada0fce25
     BUILD_IN_SOURCE true
     CONFIGURE_COMMAND
-        "${BASH_EXECUTABLE}" "${AUTOTOOLS_HELPER_AUTOGEN_CONFIGURE_MSYS}" "<SOURCE_DIR>" ${EP_AUTOTOOLS_ARGS} --disable-builddir --disable-multi-os-directory --enable-pax_emutramp
+        "${BASH_EXECUTABLE}" "${AUTOTOOLS_HELPER_MSYS}" "<SOURCE_DIR>" ./configure ${EP_AUTOTOOLS_ARGS} --disable-builddir --disable-multi-os-directory --enable-pax_emutramp
     BUILD_COMMAND ${Make_EXECUTABLE} -j${NPROC} all
     INSTALL_COMMAND ${Make_EXECUTABLE} install
 )
