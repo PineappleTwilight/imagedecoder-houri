4c2d3d78142b38a2c0b28617e99de74a23739bec
Amnesia <amnesia@example.com>
2026-09-08 11:40:14 -0400

    fix(cmake): use /usr/bin/meson|bash|make explicitly to avoid MSYS /mnt/c/msys64
    wsl meson was resolving to /mnt/c/msys64/usr/bin/meson (MSYS) via
    Windows PATH appended to WSL PATH, failing with cannot execute.
    Force /usr/bin/meson, /usr/bin/bash, /usr/bin/make for WSL. Also
    fix lcms2/libffi BUILD/INSTALL to use /usr/bin/make and add
    MAKE=/usr/bin/make + --disable-dependency-tracking for autotools
    bootstrapping.

--- library/src/main/cpp/cmake/lcms2.cmake
+++ library/src/main/cpp/cmake/lcms2.cmake
@@ -5,12 +5,12 @@ ExternalProject_Add(ep_lcms2
     URL_HASH SHA256=49e7e134e4299733dd0eda434fa468997a28ab3d33fa397c642b03644f552216
     BUILD_IN_SOURCE true
     CONFIGURE_COMMAND
-        ${CMAKE_COMMAND} -E env "CC=${_cc_msys}" "CFLAGS=--target=${ANDROID_TARGET}${ANDROID_PLATFORM_LEVEL}" "CXX=${_cxx_msys}" "CXXFLAGS=--target=${ANDROID_TARGET}${ANDROID_PLATFORM_LEVEL}" "AR=${_ar_msys}" "LD=${_ld_msys}" "RANLIB=${_ranlib_msys}" "STRIP=${_strip_msys}"
-        ${BASH_CMD} "${AUTOTOOLS_HELPER_MSYS}" "<SOURCE_DIR>" ./configure "--host=${ANDROID_TARGET}" "--prefix=${EP_AUTOTOOLS_PREFIX}" --disable-shared --enable-static --with-pic
+        ${CMAKE_COMMAND} -E env "CC=${_cc_msys}" "CFLAGS=--target=${ANDROID_TARGET}${ANDROID_PLATFORM_LEVEL}" "CXX=${_cxx_msys}" "CXXFLAGS=--target=${ANDROID_TARGET}${ANDROID_PLATFORM_LEVEL}" "AR=${_ar_msys}" "LD=${_ld_msys}" "RANLIB=${_ranlib_msys}" "STRIP=${_strip_msys}" "MAKE=/usr/bin/make"
+        ${BASH_CMD} "${AUTOTOOLS_HELPER_MSYS}" "<SOURCE_DIR>" ./configure "--host=${ANDROID_TARGET}" "--prefix=${EP_AUTOTOOLS_PREFIX}" --disable-shared --enable-static --with-pic --disable-dependency-tracking
     BUILD_COMMAND
         ${CMAKE_COMMAND} -E env "CC=${_cc_msys}" "CFLAGS=--target=${ANDROID_TARGET}${ANDROID_PLATFORM_LEVEL}" "CXX=${_cxx_msys}" "CXXFLAGS=--target=${ANDROID_TARGET}${ANDROID_PLATFORM_LEVEL}" "AR=${_ar_msys}" "LD=${_ld_msys}" "RANLIB=${_ranlib_msys}" "STRIP=${_strip_msys}"
-        ${BASH_CMD} "${AUTOTOOLS_HELPER_MSYS}" "<SOURCE_DIR>" make -j${NPROC} all
+        ${BASH_CMD} "${AUTOTOOLS_HELPER_MSYS}" "<SOURCE_DIR>" /usr/bin/make -j${NPROC} all
     INSTALL_COMMAND
         ${CMAKE_COMMAND} -E env "CC=${_cc_msys}" "CFLAGS=--target=${ANDROID_TARGET}${ANDROID_PLATFORM_LEVEL}" "CXX=${_cxx_msys}" "CXXFLAGS=--target=${ANDROID_TARGET}${ANDROID_PLATFORM_LEVEL}" "AR=${_ar_msys}" "LD=${_ld_msys}" "RANLIB=${_ranlib_msys}" "STRIP=${_strip_msys}"
-        ${BASH_CMD} "${AUTOTOOLS_HELPER_MSYS}" "<SOURCE_DIR>" make -j${NPROC} install
+        ${BASH_CMD} "${AUTOTOOLS_HELPER_MSYS}" "<SOURCE_DIR>" /usr/bin/make -j${NPROC} install
 )
